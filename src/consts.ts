4d4119da5f48d8413a8988edd534a8bec22c99c1
originalarchive <84101467+originalarchive@users.noreply.github.com>
2026-01-18 16:43:54 +0000

    Update site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,40 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "original archive",
+  EMAIL: "ken@originalarchive.net",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  TITLE: "home",
+  DESCRIPTION: "original archive by Ken Marlow is a showcase for my portfolio and articles.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
+  TITLE: "articles",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
+  TITLE: "work",
   DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  TITLE: "photography",
+  DESCRIPTION: "A collection of my image galleries and my main portfolio.",
 };
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    NAME: "reddit",
+    HREF: "https://www.reddit.com/user/Originalarchive/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button",
   },
   { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
-  },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    NAME: "threads",
+    HREF: "https://www.threads.com/@originalarchive?igshid=NTc4MTIwNjQ2YQ==",
   }
 ];
